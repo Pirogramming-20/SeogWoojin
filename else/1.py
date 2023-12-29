ce854e17ab907f614307606ad7b67c98e04cb1ee
woojin <zaza0804@uos.ac.kr>
2023-12-29 17:50:50 +0900

    코딩도장 15강

--- else/1.py
+++ else/1.py
@@ -1,10 +1,4 @@
-a,b,c,d=map(int,input().split())
+n=int(input())
 
-if(0<=a and a<=100 and 0<=b and b<=100 and 0<=c and c<=100 and 0<=d and d<=100):
-    hap=(a+b+c+d)/4
-    if hap>=80:
-        print("합격")
-    else:
-        print("불합격")
-else:
-    print("잘못된 점수")
+for i in range(1,10):
+    print(n,'*',i,'=',n*i)
