ac2b0f40a09b472ef87f1191dea8f651655aa4a0
woojin <zaza0804@uos.ac.kr>
2023-12-29 17:41:49 +0900

    코딩도장 14강

--- else/1.py
+++ else/1.py
@@ -1,4 +1,10 @@
-money=int(input())
-coupon=input()
-coupon=int(coupon[4:])
-print(money-coupon)
+a,b,c,d=map(int,input().split())
+
+if(0<=a and a<=100 and 0<=b and b<=100 and 0<=c and c<=100 and 0<=d and d<=100):
+    hap=(a+b+c+d)/4
+    if hap>=80:
+        print("합격")
+    else:
+        print("불합격")
+else:
+    print("잘못된 점수")
