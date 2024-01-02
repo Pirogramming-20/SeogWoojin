d94fd9c648fda22de0ce68d7cd7cb5890fea1c40
woojin <zaza0804@uos.ac.kr>
2024-01-02 21:59:10 +0900

    JavaScript - display items (1-6)

--- Javascript 쇼핑몰 미니게임 과제/src/main.js
+++ Javascript 쇼핑몰 미니게임 과제/src/main.js
@@ -1,8 +1,26 @@
+//fetch the items from JSON file
 function loadItems(){
     return fetch('data/data.json')
     .then(response => response.json())
     .then(json => json.items);
+}
 
+//update the list with the given items
+function displayItems(items){
+    const container=document.querySelector('.items');
+    const html=items.map(item=>createHTMLString(item));
+    console.log(html)
+    container.innerHTML = items.map(item=>createHTMLString(item)).join('');
+}
+
+// Create HTML list from the given item
+function createHTMLString(item){
+    return `
+    <li class="item">
+        <img src="${item.image}" alt="${item.type}" class="item_thumbnail" />
+        <span class="item__description">${item.gender}, ${item.size}</span>
+    </li>
+    `;
 }
 
 
@@ -10,7 +28,7 @@ function loadItems(){
 loadItems()
 .then(items=>{
     console.log(items);
-    // displayItems(items);
+    displayItems(items);
     // setEventListeners(items)
 })
 .catch(console.log);
