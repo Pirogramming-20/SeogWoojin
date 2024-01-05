618cf8d36b004cf1e0c888b9f1571a9e744446c0
woojin <zaza0804@uos.ac.kr>
2024-01-05 22:29:49 +0900

    코딩도장 38강

--- else/1.py
+++ else/1.py
@@ -1,27 +1,19 @@
-class Time:
-    def __init__(self, hour, minute, second):
-        self.hour = hour
-        self.minute = minute
-        self.second = second
-    
-    @staticmethod
-    def is_time_valid(time_string):
-        time=list(map(int,time_string.split(':')))
-        if time[0]<=24 and time[1]<60 and time[2]<=60:
-            return True
-        else:
-            return False
-        
-    @classmethod
-    def from_string(cls,time_string):
-        time=list(map(int,time_string.split(':')))
-        t=cls(time[0],time[1],time[2])
-        return t
+def palindrome(word):
+    start=0
+    end=len(word)-1
+    while(start<=end):
+        if word[start] != word[end]:
+            raise NotPalindromeError
+        start+=1
+        end-=1
+    print(word)
+class NotPalindromeError(Exception):
+    def __init__(self):
+        super().__init__("회문이 아닙니다.")
 
-time_string = input()
- 
-if Time.is_time_valid(time_string):
-    t = Time.from_string(time_string)
-    print(t.hour, t.minute, t.second)
-else:
-    print('잘못된 시간 형식입니다.')
+
+try:
+    word = input()
+    palindrome(word)
+except NotPalindromeError as e:
+    print(e)
